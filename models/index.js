3172257d2c2f660a5282dc74ca75ef178fa13203
bdurham227 <bdurham227@gmail.com>
2021-05-02 20:00:51 -0400

    force dropped created and re seeded database bc i picked up error in index.js with the associations, i had tag_id and product_id backwards so it wasnt creating the correct association

--- models/index.js
+++ models/index.js
@@ -12,7 +12,7 @@ Product.belongsTo(Category, {
 // Categories have many Products
 Category.hasMany(Product, {
   foreignKey: 'category_id',
-  onDelete: "CASCADE",
+  
 });
 
 // Products belongToMany Tags (through ProductTag)
@@ -20,7 +20,7 @@ Product.belongsToMany(Tag, {
   through: {
     model: ProductTag,
   },
-  foreignKey: 'tag_id'
+  foreignKey: 'product_id'
   
 })
 
@@ -30,7 +30,7 @@ Tag.belongsToMany(Product, {
   through: {
     model: ProductTag
   },
-  foreignKey: 'product_id'
+  foreignKey: 'tag_id'
 })
 
 module.exports = {
